15627609e328c68d19bb8573eea0ed873635166b
Hassan Daar <70281654+daarhassan@users.noreply.github.com>
2023-04-29 04:39:56 +0500

    Add files via upload

---
+++ frontend/src/utils/AxiosJWT.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const baseURL = "https://pawspets.onrender.com/";
+
+var loginToken = localStorage.getItem('jwtToken');
+console.log("Token: "+loginToken);
+
+const axiosInstance = axios.create({
+    baseURL: baseURL,
+    timeout: 5000,
+    headers: {
+        'Authorization': loginToken ? (loginToken ?  "Bearer " + loginToken: null) : null,
+        'Content-Type': 'application/json',
+        'accept': 'application/json'
+    }
+});
+
+export default axiosInstance;
